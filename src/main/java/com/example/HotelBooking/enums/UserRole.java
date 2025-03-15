0020bcb9abd2d14b5b03fa2e94394533780315fa
Dip Biswas <bdip6333@gmail.com>
2025-03-16 00:47:42 +0530

    Initial commit

---
+++ src/main/java/com/example/HotelBooking/enums/UserRole.java
@@ -0,0 +1,6 @@
+package com.example.HotelBooking.enums;
+
+public enum UserRole {
+    CUSTOMER,
+    ADMIN
+}
---
+++ src/main/java/com/example/HotelBooking/exceptions/NameValueRequiredException.java
@@ -0,0 +1,8 @@
+package com.example.HotelBooking.exceptions;
+
+public class NameValueRequiredException extends RuntimeException {
+    public NameValueRequiredException(String message)
+    {
+        super(message);
+    }
+}
