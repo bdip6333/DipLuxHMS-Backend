a7ecec6186854aaed58791a90067736ed7ea69c9
Dip Biswas <bdip6333@gmail.com>
2025-03-16 15:19:06 +0530

    Initial commit

--- src/main/java/com/example/HotelBooking/security/CorsConfig.java
+++ src/main/java/com/example/HotelBooking/security/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "dip-lux-hms.vercel.app")
+                .allowedOrigins("http://localhost:3000", "https://dip-lux-hms.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
